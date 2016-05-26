d3b99030c9111344c7699c2d0ebf41c1cf8fd595
Evan Darke <edarke@ucsd.edu>
2016-05-25 18:45:45 -0700

    improved coverage of ternary_basic

--- ternary_basic.glsl
+++ ternary_basic.glsl
@@ -1,17 +1,28 @@
 //precision highp float;   
 
-int test(){
+bool test(){
+  bool checkpoint = true;
+  
   int x = true ? 1 : 0;
   int y = false? 1 : 0;
   
-  return x*10 + y; 
+  checkpoint = x == 1 && checkpoint && y == 0;
+  
+  checkpoint = checkpoint ? true : false;
+  
+  float f = 1.0;
+  float f = true ? 2.0 : 3.0;
+  checkpoint = checkpoint && f == 2.0;
+  
+  
+  return checkpoint; 
 }
-// Expected output: 10
+// Expected output: -1
 
 /*
 void main()
 {
-  if(test() == 10) // Assert function returns expected value
+  if(test()) // Assert function returns expected value
     gl_FragColor = vec4(0,1,0,1.0); // green
   else
     gl_FragColor = vec4(1,0,0,1.0);  // red
